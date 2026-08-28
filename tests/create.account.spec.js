7646f04632b59e796d1c37bb171d308f5ffc1320
Steven Segaud <steven.segaud@gmail.com>
2026-08-28 08:26:51 +0100

    corrected .click()

--- tests/create.account.spec.js
+++ tests/create.account.spec.js
@@ -46,12 +46,12 @@ test('create new UK test account', async ({page}) => {
     await page.getByLabel('Given Name').fill('Steven');
     await page.getByLabel('Family Name').fill('Segaud');
     // 1. Focus the dropdown input
-    await page.getByRole('button', {name: /select farm location/i}).click;
+    await page.getByRole('button', {name: /select farm location/i}).click();
     // 2. Select the option from dropdown
-    await page.getByText('(UK) United Kingdom').click;
+    await page.getByText('(UK) United Kingdom').click();
     await page.getByLabel('Phone Number').fill('07934108770');
-    await page.getByRole('button', {name: /Did you buy hardware?/i}).click;
-    await page.getByText('Yes').click
+    await page.getByRole('button', {name: /Did you buy hardware?/i}).click();
+    await page.getByText('Yes').click()
     await page.click('button[type="submit"]');
 
     const pricingFrame = page.frameLocator('stripe-pricing-table iframe[src*="pricing-table-app"]');
